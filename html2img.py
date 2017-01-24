173011221c446cf62f938c033cea092b3553aad5
onionlee <rkdgml6332@gmail.com>
2017-01-24 10:55:31 +0900

    예외처리

--- html2img.py
+++ html2img.py
@@ -3,12 +3,17 @@ import codecs
 from bs4 import BeautifulSoup
 import urllib.request
 
-print("type filename")
-filename = input()
-dic = os.path.dirname( os.path.abspath( __file__ ) )
-path = dic + "/" + filename
-f = codecs.open(path, "r", "utf-8")
-
+while True:
+    try:
+        print("type filename")
+        filename = input()
+        dic = os.path.dirname( os.path.abspath( __file__ ) )
+        path = dic + "/" + filename
+        f = codecs.open(path, "r", "utf-8")
+        break
+    except Exception as e:
+        print(e)
+    
 html_doc = f.read()
 f.close()
 
@@ -16,7 +21,12 @@ soup = BeautifulSoup(html_doc, "html.parser")
 page_images = [image["src"] for image in soup.findAll("img")]
 count = 0
 for img in page_images:
+    count = count + 1
     split = img.split("?type")
-    print(split[0])
-    urllib.request.urlretrieve(split[0], split[0].split('/')[-1])
-   
+    try:
+        urllib.request.urlretrieve(split[0], split[0].split('/')[-1])
+        print("[{0}/{1}]{2}".format(count, len(page_images), split[0]))
+    except Exception as e:
+        print("[{0}/{1}]{2}".format(count, len(page_images), e))
+
+input()
